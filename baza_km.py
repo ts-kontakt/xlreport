2c5f273fe8f0847134f3bfbfe754d8e32e41a32d
ts-kontakt <ts.kontakt@gmail.com>
2014-11-28 09:13:42 +0100

    Added line of code
    just test test

--- baza_km.py
+++ baza_km.py
@@ -1,4 +1,6 @@
 import sqlitehelper as sqh
+#example commits
+
 con, cur = sqh.get_con_cur('c:\\1dane\\baza_sprzed.sdb')
 def get_km():
     outdict = {}
@@ -8,5 +10,8 @@ def get_km():
     return outdict
     
 if __name__ == '__main__': 
+    print 1
     print get_km()
+    
+
     
