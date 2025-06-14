a697dcb384fb6d73e43393149b50f828bce4689a
Tomasz <ts.kontakt@gmail.com>
2025-06-14 19:02:50 +0200

    Support for 1-d data

--- xlreport.py
+++ xlreport.py
@@ -7,12 +7,13 @@ import os
 import re
 import subprocess
 import sys
+from itertools import zip_longest
 from math import log
 
 import xlsxwriter as xls
 
 HEADER_FONT_NAME = "Times"
-GEN_FONT_NAME = "Arial" 
+GEN_FONT_NAME = "Arial"
 FONT_SIZE = 9
 WRAP_FONT_SIZE = 9
 TITLE_RANGE = "B1:E1"
@@ -20,6 +21,7 @@ HEADER_BG_COLOR = "#D4D0C8"
 HEADER_FONT_COLOR = "#003366"
 NUM_FORMAT = "0.00;[RED]-0.00"
 TITLE_BG = "#F1F1F1"
+
 
 def ensure_unicode(input_value):
     if "bytes" in repr(type(input_value)):
@@ -35,15 +37,32 @@ def open_file(filename):
         subprocess.call([opener, filename])
 
 
+def is_nested(inlist):
+    first = None
+    nested = True
+    try:
+        first = next(i for i in inlist)
+        print(first)
+    except TypeError:
+        nested = False
+    if 'str' in repr(type(first)):
+        nested = False
+    else:
+        try:
+            next(i for i in first)
+        except TypeError:
+            nested = False
+    return nested
+
+
 class Exfile:
+
     def __init__(self, filename):
         assert ".xlsx" in filename
         filename.upper()
         self.workbook = xls.Workbook(filename)
 
-        file_path = (
-            filename if os.sep in filename else os.path.join(os.getcwd(), filename)
-        )
+        file_path = (filename if os.sep in filename else os.path.join(os.getcwd(), filename))
         self.file_path = file_path
 
     @staticmethod
@@ -53,19 +72,21 @@ class Exfile:
 
         if text_length <= 3:
             return 3
-        return (
-            ((51.16 * log(text_length) - 38.395) * 0.85) / 10
-            + (log(text_length) * 2)
-            + extra_width
-        )
+        return (((51.16 * log(text_length) - 38.395) * 0.85) / 10 + (log(text_length) * 2) +
+                extra_width)
 
     def write(self, data_list, title, worksheet_name=None, wrap=False):
         datatype = repr(type(data_list)).lower()
+        print(datatype)
         assert re.search("list|dict_values|dataframe|array|tuple|set", datatype)
+
         if "dataframe" in datatype:
             new_list = [["idx"] + list(data_list.columns)]
             new_list.extend(data_list.to_records())
             data_list = new_list
+        else:
+            if not is_nested(data_list):
+                data_list = ['h'] + list(zip_longest(data_list, fillvalue=[]))
 
         title.upper()
         if worksheet_name:
@@ -77,17 +98,15 @@ class Exfile:
         start_row = 4
         start_column = 1
 
-        merge_format = self.workbook.add_format(
-            {
-                "bold": 1,
-                "border": 1,
-                "align": "center",
-                "fg_color": TITLE_BG,
-                "text_wrap": True,
-                "font_name": HEADER_FONT_NAME,
-                "font_size": 12,
-            }
-        )
+        merge_format = self.workbook.add_format({
+            "bold": 1,
+            "border": 1,
+            "align": "center",
+            "fg_color": TITLE_BG,
+            "text_wrap": True,
+            "font_name": HEADER_FONT_NAME,
+            "font_size": 12,
+        })
 
         worksheet.merge_range(TITLE_RANGE, ensure_unicode(title), merge_format)
 
@@ -98,29 +117,28 @@ class Exfile:
         header_format.set_bg_color(HEADER_BG_COLOR)
         header_format.set_indent(1)
 
-        cell_format = self.workbook.add_format(
-            {
-                "font_name": GEN_FONT_NAME,
-                "font_size": FONT_SIZE,
-                "num_format": number_format,
-            }
-        )
+        cell_format = self.workbook.add_format({
+            "font_name": GEN_FONT_NAME,
+            "font_size": FONT_SIZE,
+            "num_format": number_format,
+        })
 
-        decimal_format = self.workbook.add_format(
-            {"font_size": FONT_SIZE, "num_format": NUM_FORMAT}
-        )
+        decimal_format = self.workbook.add_format({
+            "font_size": FONT_SIZE,
+            "num_format": NUM_FORMAT
+        })
 
-        long_text_format = self.workbook.add_format(
-            {"text_wrap": True, "font_size": WRAP_FONT_SIZE}
-        )
+        long_text_format = self.workbook.add_format({
+            "text_wrap": True,
+            "font_size": WRAP_FONT_SIZE
+        })
         long_text_format.set_align("vcenter")
 
         for column_index, header_value in enumerate(data_list[0]):
             text_length = len(repr(header_value))
             column_width = self.calculate_column_width(text_length)
-            worksheet.set_column(
-                start_column + column_index, start_column + column_index, column_width
-            )
+            worksheet.set_column(start_column + column_index, start_column + column_index,
+                                 column_width)
 
         worksheet.freeze_panes(start_row, 0)
 
@@ -161,9 +179,7 @@ class Exfile:
                         )
                 else:
                     try:
-                        if abs(cell_value) < 20.0 and int(cell_value) != round(
-                            cell_value, 2
-                        ):
+                        if abs(cell_value) < 20.0 and int(cell_value) != round(cell_value, 2):
                             worksheet.write(
                                 current_row,
                                 start_column + column_index,
@@ -192,14 +208,12 @@ class Exfile:
         start_row = 0
         worksheets = self.workbook.worksheets()
 
-        inactive_format = self.workbook.add_format(
-            {
-                "font_color": "gray",
-                "bold": 0,
-                "underline": 1,
-                # "font_size": 10,
-            }
-        )
+        inactive_format = self.workbook.add_format({
+            "font_color": "gray",
+            "bold": 0,
+            "underline": 1,
+            # "font_size": 10,
+        })
 
         for source_sheet in worksheets:
             link_index = 0
@@ -208,7 +222,7 @@ class Exfile:
                     source_sheet.write_url(
                         start_row,
                         start_column + link_index,
-                        f"internal:'{target_sheet.name}'!A1"  ,
+                        f"internal:'{target_sheet.name}'!A1",
                         inactive_format,
                         string=target_sheet.name,
                     )
@@ -216,7 +230,7 @@ class Exfile:
                     source_sheet.write_url(
                         start_row,
                         start_column + link_index,
-                        f"internal:'{target_sheet.name}'!A1"  ,
+                        f"internal:'{target_sheet.name}'!A1",
                         string=target_sheet.name,
                     )
                 link_index += 1
@@ -236,9 +250,7 @@ class Exfile:
             pass
 
 
-def to_file(
-    xls_name, inlist, header_list=None, title="Title", shname="sheet1", wrap=False
-):
+def to_file(xls_name, inlist, header_list=None, title="Title", shname="sheet1", wrap=False):
     assert "str" in repr(type(xls_name))
     exfile = Exfile(xls_name)
     if header_list:
@@ -306,7 +318,6 @@ def generate_random_data(num_rows=10):
 
 def test_numpy():
     from numpy.random import default_rng
-
     arr = default_rng(42).random((100, 4))
     header = ["col1", "col2", "col3", "col4"]
     to_file("test.xlsx", arr, header, title="Test numpy")
@@ -316,17 +327,20 @@ def test_df():
     import numpy as np
     import pandas as pd
 
-    df = pd.DataFrame(
-        {
-            "col1": ["A", "A", False, np.nan, "D", "C"],
-            "col2": [2, 1, 9, -8, 7, -4],
-            "col3": [-0.8, 1, 9, 4, 2, 3],
-            "col4": ["a", "B", "c", "D", True, "F"],
-        }
-    )
+    df = pd.DataFrame({
+        "col1": ["A", "A", False, np.nan, "D", "C"],
+        "col2": [2, 1, 9, -8, 7, -4],
+        "col3": [-0.8, 1, 9, 4, 2, 3],
+        "col4": ["a", "B", "c", "D", True, "F"],
+    })
+    df = list(range(100))
     to_file("testdf.xlsx", df, title="Test dataframe")
 
 
+def test_1d():
+    r = list(range(100))
+    to_file("simplelist.xlsx", r, title="Simple list")
+
+
 if __name__ == "__main__":
-    # test_numpy()
     test_df()
